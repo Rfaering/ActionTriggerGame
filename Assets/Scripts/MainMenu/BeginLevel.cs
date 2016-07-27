8534c6019047d8ba07f9ffd08b6955d916c84a9a
Rasmus Færing Larsen <rfaering@gmail.com>
2016-07-27 22:55:30 +0200

    Added background images

---
+++ Assets/Scripts/MainMenu/BeginLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BeginLevel : MonoBehaviour {
+
+	public void BeginGame()
+    {
+        SceneManager.LoadSceneAsync("Game");
+    }
+}
